f379fd3f0b765a43ba2baae8522bd44076ab555b
ogaroh <ogaroerick1998@gmail.com>
2024-05-10 19:42:54 +0300

    feat: icon

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moniepoint_test/app/theme/theme.dart';
 import 'package:moniepoint_test/home/views/pages/default_scaffold_page.dart';
-// import 'package:moniepoint_test/home/views/pages/search.dart';
 import 'package:moniepoint_test/l10n/l10n.dart';
 
 class App extends StatelessWidget {
@@ -15,11 +14,10 @@ class App extends StatelessWidget {
     final theme = MaterialTheme(textTheme);
 
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: brightness == Brightness.light ? theme.light() : theme.dark(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      // home: const HomePage(),
-      // home: const SearchPage(),
       home: const DefaultScaffoldPage(),
     );
   }
